fbd946ae66e0e4ccdb67f36e752356ff8210dbde
deweidemann <deweidemann@bio.miami.edu>
2014-01-28 12:20:27 -0500

    changed values to integers

--- solar_radiation_data.R
+++ solar_radiation_data.R
@@ -8,3 +8,14 @@ x<-2*4
 #test to see if "2" and "4" are integers
 (is.integer(2))
 (is.integer(4))
+# not integers so make them integers
+
+x <- 2
+x<-as.integer(x)
+is.integer(x)
+
+y<- 4
+y<-as.integer(y)
+is.integer(y)
+
+(z<-x*y)
